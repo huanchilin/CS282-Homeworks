59bd828cec75d5638150cb60df16afefbeaa09ed
huancilin <huanchilin@gmail.com>
2017-05-31 13:30:22 +0800

    Lecture02-Examples

---
+++ Lecture02-Examples/Example01/Example03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string html =
+                @"select * from Table
+                where id >10
+                ";
+            Console.WriteLine(html);
+            string path = @"C:\xampp\htdoc\";
+            Console.WriteLine(path);
+        }
+    }
+}
---
+++ Lecture02-Examples/Example01/Example05/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example05
+{
+    public class Person
+    {
+        public string Name;
+        public string Birthday;
+        public string Email;
+        public Position Pos;
+        public double Orientation;
+
+        public void MoveForward(double distance)
+        {
+            Pos.X = Pos.X + Math.Cos(Orientation * Math.PI / 180.0) * distance;
+            Pos.Y = Pos.Y + Math.Sin(Orientation * Math.PI / 180.0) * distance;
+        }
+
+        public void TurnRight(double angle)
+        {
+            Orientation = Orientation + angle;
+        }
+
+        public void ShowPosition()
+        {
+            Console.WriteLine("{0}'s position is at: ({1},{2}) Orientation at {3} degree", Name, Pos.X, Pos.Y, Orientation);
+        }
+
+        public void DrawRectRight(double length, double width)
+        {
+            for (var count = 0; count < 2; count++)
+            {
+                this.MoveForward(length);
+                this.TurnRight(90);
+                this.MoveForward(width);
+                this.TurnRight(90);
+                this.ShowPosition();
+            }
+        }
+
+        public void DrawRectRight()
+        {
+            DrawRectRight(50, 40);
+        }
+
+        public void DrawRectLeft(double length, double width)
+        {
+            for (var count = 0; count < 2; count++)
+            {
+                this.MoveForward(width);
+                this.TurnRight(90);
+                this.MoveForward(length);
+                this.TurnRight(90);
+                this.ShowPosition();
+            }
+        }
+
+        public void DrawRectLeftt()
+        {
+            DrawRectLeft(50, 40);
+        }
+    }
+}
