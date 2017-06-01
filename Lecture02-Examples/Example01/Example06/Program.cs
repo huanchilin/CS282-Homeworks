0d1beffef8622ac5774e3501863375cf58beb06e
huancilin <huanchilin@gmail.com>
2017-06-01 21:25:54 +0800

    Lecture02

---
+++ Lecture02-Examples/Example01/Example06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example06
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Product[] products = new Product[4];
+            products[0] = new Product()
+            {
+                Name = "iPhone 7",
+                Price = 28000
+            };
+            products[1] = new Product()
+            {
+                Name = "iPhone 6",
+                Price = 2800
+            };
+            products[2] = new Product()
+            {
+                Name = "iPhone 5",
+                Price = 280
+            };
+            products[3] = new Product()
+            {
+                Name = "iPhone 4",
+                Price = 28
+            };
+            Random random = new Random();
+            List<Product> shoppingCart = new List<Product>();
+            for (var count = 1; count <= 10; count++)
+            {
+                var index = random.Next(0, products.Length);
+                shoppingCart.Add(products[index]);
+                Console.WriteLine(products[index].Name);
+            }
+        }
+    }
+}
